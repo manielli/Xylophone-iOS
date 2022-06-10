df3b70e1598b0205c5131706500175e507015af4
Mani Jafari <contact@manijafari.com>
2022-06-09 17:19:23 -0700

    Add a interface builder action for C button and pass in the "C" wave file as the url for the AVAudioPlayer object to play that sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,17 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
+        playSound()
     }
     
-    
-
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
